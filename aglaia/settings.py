dc4efb39194b833d896913746b7cfd9a4e51cbe0
zhangmingzhao <zhangmz13@mails.tsinghua.edu.cn>
2015-10-22 14:16:13 +0800

    消除1215错误

--- aglaia/settings.py
+++ aglaia/settings.py
@@ -68,6 +68,7 @@ DATABASES = {
          'PASSWORD': 'firstmysql',
          'HOST': '127.0.0.1',
          'PORT': '3306',
+         'OPTIONS': {"init_command": "SET foreign_key_checks = 0;", },
      }
 }
 
