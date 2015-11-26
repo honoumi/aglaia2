20ef61c65fe6d81804802e0475ca9082fa431a6c
zhangmingzhao <zhangmz13@mails.tsinghua.edu.cn>
2015-11-26 10:14:31 +0800

    适配虚拟机

--- aglaia/settings.py
+++ aglaia/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
          'ENGINE': 'django.db.backends.mysql',
          'NAME': 'aglaia',
          'USER': 'root',
-         'PASSWORD': 'firstmysql',
+         'PASSWORD': 'vone_58271508&toki@first',
          'HOST': '127.0.0.1',
          'PORT': '3306',
          'OPTIONS': {"init_command": "SET foreign_key_checks = 0;", },
